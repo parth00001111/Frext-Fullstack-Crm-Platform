fd6acac0bf86626c3c2d0442b83972cae0f9637c
Parth <kanhamahajan73@gmail.com>
2026-07-26 02:02:02 +0530

    added more models

--- backend/src/Models/customerModel.ts
+++ backend/src/Models/customerModel.ts
@@ -1,5 +1,4 @@
 import mongoose, { model, Schema, Document } from "mongoose"; 
-
 export interface ICustomer extends Document {
     name: string,
     email: string,
@@ -8,44 +7,29 @@ export interface ICustomer extends Document {
     status: string, 
     assignedTo: mongoose.Types.ObjectId,
 }
-
 const customerSchema = new Schema<ICustomer>({
     name: {
         type: String, 
         required: true, 
-        
         trim: true,
-    }, 
-
-    email: {
+    }, email: {
         type: String, 
         unique: true,
         required: true,
         trim: true, 
-        
-    },
-
-    phone: {
+    },phone: {
         type: String, 
         required: true,
         unique: true,
         trim: true,
-    },
-
-    company: {
+    },company: {
         type: String,
         trim: true, 
-        
-    },
-
-    status: {
+    },status: {
         type: String,
         enum: ['new', 'contacted', 'closed'],
         default: "new",
-
-    },
-
-    assignedTo: {
+    },assignedTo: {
         type: Schema.Types.ObjectId, 
         ref: "User",
         required: true,
